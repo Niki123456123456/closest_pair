3263f8ec02531bc70c10a3fdccc7ba78743c9f5f
Niki123456123456 <85129772+Niki123456123456@users.noreply.github.com>
2024-09-23 21:06:50 +0200

    fix names

--- assets/sw.js
+++ assets/sw.js
@@ -2,8 +2,8 @@ var cacheName = 'egui-template-pwa';
 var filesToCache = [
   './',
   './index.html',
-  './eframe_template.js',
-  './eframe_template_bg.wasm',
+  './closest_pair.js',
+  './closest_pair_bg.wasm',
 ];
 
 /* Start the service worker and cache all of the app's content */
